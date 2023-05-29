367c3ff4c04b8df260f08c6d504a574c728ad34f
Adam Ezzaim <adam.ezzaim@epfl.ch>
2023-05-29 16:03:55 +0200

    add data pre proccessing
    added the data, cleaned the text and split into test and train data

---
+++ predictor/data/DataPreProcess.py
@@ -0,0 +1,43 @@
+import string
+import re
+import argparse
+from sklearn.model_selection import train_test_split
+
+# Create the parser
+parser = argparse.ArgumentParser(description="Clean and split text data")
+
+# Add the arguments
+parser.add_argument('--file', '-f', type=str, help="The name of the data file")
+parser.add_argument('--train', '-tr', type=str, help="The name of the train data file")
+parser.add_argument('--test', '-t', type=str, help="The name of the test data file")
+
+# Parse the arguments
+args = parser.parse_args()
+
+# Read the data from the file
+with open(args.file, 'r') as file:
+    data = file.read()
+
+# Split data into paragraphs
+paragraphs = data.split('\n')
+
+# Clean each paragraph
+for i in range(len(paragraphs)):
+    # Remove anything that isn't a space, an apostrophe, or a letter
+    paragraphs[i] = re.sub(r"[^a-zA-Z' ]+", '', paragraphs[i])
+    # Replace extra whitespace
+    paragraphs[i] = re.sub(r'\s+', ' ', paragraphs[i]).strip()
+
+# Split the paragraphs into training and testing data
+train_data, test_data = train_test_split(paragraphs, test_size=0.2, random_state=42)
+
+# Write the training data to a file
+with open(args.train, 'w') as file:
+    for paragraph in train_data:
+        file.write(paragraph + '\n')
+
+# Write the testing data to a file
+with open(args.test, 'w') as file:
+    for paragraph in test_data:
+        file.write(paragraph + '\n')
+
